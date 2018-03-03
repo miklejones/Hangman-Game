82c64a445b261f958b3317f5cdb0be20d1b903ec
Michael Garland <michaelcgarland@gmail.com>
2018-03-03 01:51:37 -0500

    so much closer with things actually working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,27 @@
-//Set variables for stats
-var wins = 0;
-var losses = 0;
-var guessesLeft = 0;
-
 //Set variables for words and alphabet
-var vocab = ['cheeseesese', 'qwerqwertyuityuiopopasdfasdfgjkhjkllzxcvzxcvvbmnvmnasgfkljgasdjgqweuiyweiqtkcvb'];
-var randWord = vocab[Math.floor(Math.random() * vocab.length)];
+var vocab = ['coffee', 'juice', 'water', 'gatorade', 'lemonade', 'tea', 'beer', 'cocktail'];
+
+var randWord = "";
 var blankWord = [];
 var blankWordDisplay = "";
 var userGuess = "";
 var toReplace = 0;
+var wordArray = [];
 
-//Create array of the random word
-var wordArray = Array.from(randWord);
+//Set variables for stats
+var wins = 0;
+var losses = 0;
+var wrongGuesses = [];
+var guessesLeft = 13;
 
-//Var for correct guess
-var hit = false;
-
-
-
-//Function to restart blanks after win or loss
-function showBlanks() {
-
-
-
-    //Count number of items within the array and add them to the blank array as underscores
+function startGame() {
+    guessesLeft = 13;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    blankWord = []
+    blankWordDisplay = "";
+    document.getElementById("play-area").innerHTML = blankWordDisplay;
+    randWord = vocab[Math.floor(Math.random() * vocab.length)];
+    wordArray = Array.from(randWord);
     var letterCount = wordArray.length;
     for (i = 0; i < wordArray.length; i++) {
         blankWord.push('_');
@@ -35,50 +32,61 @@ function showBlanks() {
     document.getElementById("play-area").innerHTML = blankWordDisplay;
 };
 
-//Call function
-showBlanks();
+startGame();
 
 
 //Logic to assign value to guess
 document.onkeyup = function (event) {
 
-    //Determind the guess
-    userGuess = event.key.toLowerCase();
+    //require it is a letter
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-    //Logic for guessing a correct letter
-    if (wordArray.indexOf(userGuess) > -1) {
-        console.log('Hell yeah');
-        hit = true;
+        //Determind the guess
+        userGuess = event.key.toLowerCase();
+        //Logic for guessing a correct letter
+        if (wordArray.indexOf(userGuess) > -1) {
+            console.log('Hell yeah');
+            //locate  use of the letter
+            toReplace = wordArray.indexOf(userGuess);
+            //replace the letter
+            blankWord.splice(toReplace, 1, userGuess);
+            //check for more
+            for (r = 0; r < wordArray.length; r++) {
 
-        //locate  use of the letter
-        toReplace = wordArray.indexOf(userGuess);
-        //replace the letter
-        blankWord.splice(toReplace, 1, userGuess);
+                //begin check at next interval in array
+                if (wordArray.includes(userGuess, toReplace + 1) > 0) {
+                    console.log('whoopee');
+                    toReplace = wordArray.indexOf(userGuess, toReplace + 1);
+                    //replace the letter
+                    blankWord.splice(toReplace, 1, userGuess);
+                } else {
+                    console.log('shit');
+                };
+            }
 
-        //check for more
-        for (r = 0; r < wordArray.length; r++) {
-            if (wordArray.includes(userGuess, toReplace + 1) > 0) {
-                console.log('whoopee');
-                toReplace = wordArray.indexOf(userGuess, toReplace + 1);
-                //replace the letter
-                blankWord.splice(toReplace, 1, userGuess);
-            } else {
-                console.log('shit');
-            };
-        }
+            //push to HTML
+            var blankWordShow = blankWord.join(' ');
+            //display said array
+
+            document.getElementById("play-area").innerHTML = blankWordShow;
+        } else {
+            console.log('Whoops');
+            guessesLeft--;
+            document.getElementById("guesses-left").innerHTML = guessesLeft;
+        };
 
 
-
-        //push to HTML
-        var blankWordShow = blankWord.join(' ');
-        //display said array
-        document.getElementById("play-area").innerHTML = blankWordShow;
+        //What to do when you win
+        if (blankWord.includes('_')) {
+            console.log('guess more');
+        } else {
+            console.log('you win');
+            startGame();
+        };
 
     } else {
-        console.log('Whoops');
-        hit = false;
+        alert('Press a letter actually. hehe');
     };
-
 };
 
 //If there are no '_' left in blank word then you win
