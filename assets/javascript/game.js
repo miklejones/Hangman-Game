1b6f59e9093531b7dd0f3314035b22c0a4a848cd
Michael Garland <michaelcgarland@gmail.com>
2018-03-02 22:54:28 -0500

    working mechanics in place, needs game win and lose rules

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,57 +4,81 @@ var losses = 0;
 var guessesLeft = 0;
 
 //Set variables for words and alphabet
-var vocab = ['coffee', 'juice', 'water'];
-var alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var vocab = ['cheeseesese', 'qwerqwertyuityuiopopasdfasdfgjkhjkllzxcvzxcvvbmnvmnasgfkljgasdjgqweuiyweiqtkcvb'];
 var randWord = vocab[Math.floor(Math.random() * vocab.length)];
 var blankWord = [];
+var blankWordDisplay = "";
+var userGuess = "";
+var toReplace = 0;
+
+//Create array of the random word
+var wordArray = Array.from(randWord);
 
 //Var for correct guess
 var hit = false;
 
 
-//Function to restart after win or loss
+
+//Function to restart blanks after win or loss
 function showBlanks() {
 
-    //Create array of the random word
-    var wordArray = Array.from(randWord);
+
 
     //Count number of items within the array and add them to the blank array as underscores
     var letterCount = wordArray.length;
     for (i = 0; i < wordArray.length; i++) {
         blankWord.push('_');
     }
-    blankWord = blankWord.join(' ');
+    blankWordDisplay = blankWord.join(' ');
 
     //display said array
-    document.getElementById("play-area").innerHTML = blankWord;
-}
+    document.getElementById("play-area").innerHTML = blankWordDisplay;
+};
 
+//Call function
+showBlanks();
 
-
-
-//Create array of the random word
-var wordArray = Array.from(randWord);
-//Count number of items within the array and add them to the blank array as underscores
-var letterCount = wordArray.length;
-for (i = 0; i < wordArray.length; i++) {
-    blankWord.push('_');
-}
-blankWord = blankWord.join(' ');
-//display said array
-document.getElementById("play-area").innerHTML = blankWord;
 
 //Logic to assign value to guess
 document.onkeyup = function (event) {
 
     //Determind the guess
-    var userGuess = event.key.toLowerCase();
+    userGuess = event.key.toLowerCase();
 
     //Logic for guessing a correct letter
-    if (randWord.indexOf(userGuess) > -1) {
+    if (wordArray.indexOf(userGuess) > -1) {
         console.log('Hell yeah');
+        hit = true;
+
+        //locate  use of the letter
+        toReplace = wordArray.indexOf(userGuess);
+        //replace the letter
+        blankWord.splice(toReplace, 1, userGuess);
+
+        //check for more
+        for (r = 0; r < wordArray.length; r++) {
+            if (wordArray.includes(userGuess, toReplace + 1) > 0) {
+                console.log('whoopee');
+                toReplace = wordArray.indexOf(userGuess, toReplace + 1);
+                //replace the letter
+                blankWord.splice(toReplace, 1, userGuess);
+            } else {
+                console.log('shit');
+            };
+        }
+
+
+
+        //push to HTML
+        var blankWordShow = blankWord.join(' ');
+        //display said array
+        document.getElementById("play-area").innerHTML = blankWordShow;
+
     } else {
         console.log('Whoops');
+        hit = false;
     };
 
 };
+
+//If there are no '_' left in blank word then you win
